61ff9c4bfb3d25416f30c06e6abc4171d0f02c58
Ben Copeland <bscopeland@gmail.com>
2018-06-08 22:25:26 -0400

    Convert user router to async/await

--- server/user/user.route.js
+++ server/user/user.route.js
@@ -3,11 +3,11 @@ const express = require('express');
 const router = express.Router();
 const User = require('./user');
 
-router.post('/user/create', (req, res) => {
-  User.create(req.body).then(data => res.json(data));
+router.post('/user/create', async (req, res) => {
+  res.json(await User.create(req.body));
 });
-router.post('/user/login', (req, res) => {
-  User.login(req.body).then(data => res.json(data));
+router.post('/user/login', async (req, res) => {
+  res.json(await User.login(req.body));
 });
 
 module.exports = router;
