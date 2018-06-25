b324ec5e647cd63da9212b73c7199e75892100b2
Ben Copeland <bscopeland@gmail.com>
2018-06-24 21:40:56 -0400

    Switch to reactive form

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule} from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,7 +22,6 @@ import { FblLoginComponent } from './fbl-login/fbl-login.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     BrowserModule,
-    FormsModule,
     LayoutModule,
     MatButtonModule,
     MatCardModule,
@@ -34,6 +33,7 @@ import { FblLoginComponent } from './fbl-login/fbl-login.component';
     MatProgressSpinnerModule,
     MatSidenavModule,
     MatToolbarModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/fbl-login/fbl-login.component.ts
+++ client/src/app/fbl-login/fbl-login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { patternValidator } from '../services/pattern-validator.service';
 
 @Component({
   selector: 'fbl-fbl-login',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fbl-login.component.scss']
 })
 export class FblLoginComponent implements OnInit {
+  loginForm: FormGroup;
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit () {
+    this.createForm();
+  }
+
+  private createForm () {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, patternValidator(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)]),
+      password: new FormControl('', Validators.required),
+    });
+  }
+
+  login () {
+    console.log(this.loginForm.value);
   }
 
 }
